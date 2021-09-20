22917c2055a10f4866e0f39b684b4962b5bec4de
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-09-20 16:52:56 -0500

    cleaned up code with sudocode

--- assets/css/js/single.js
+++ assets/css/js/single.js
@@ -1,46 +1,30 @@
 var repoNameEl = document.querySelector("#repo-name");
 var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
-var repoNameEl = document.querySelector("#repo-name");
-
-var getRepoName = function() {
-    // grab repo name from url query string
-    var queryString = document.location.search;
-    var repoName = queryString.split("=")[1];
-  
-    if (repoName) {
-      // display repo name on the page
-      repoNameEl.textContent = repoName;
-  
-      getRepoIssues(repoName);
-    } else {
-      // if no repo was given, redirect to the homepage
-      document.location.replace("./index.html");
-    }
-  };
-
-
 
 var getRepoIssues = function(repo) {
+    // format the github api url
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    console.log(repo);
+  
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
-        // request was successful
-        if (response.ok) {
-          response.json().then(function(data) {
-            // pass response data to dom function
-            displayIssues(data);
-            // check if api has paginated issues
-    if (response.headers.get("Link")) {
-        displayWarning(repo)
+      // request was successful
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayIssues(data);
+  
+          // check if api has paginated issues
+          if (response.headers.get("Link")) {
+            displayWarning(repo);
+          }
+        });
+      } else {
+        // if not successful, redirect to homepage
+        document.location.replace("./index.html");
       }
     });
-        }
-        else {
-            document.location.replace("./index.html");
-        }
-      });
-};
+  };
+
 
 
 var displayIssues = function(issues) {
